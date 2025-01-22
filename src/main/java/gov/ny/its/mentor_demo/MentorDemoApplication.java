d024af704dc0bd07bdb1e4cafa75ce7b8f2e8f28
Christopher Walsh <chris.is.rad@pm.me>
2025-01-22 15:06:46 -0500

    set up OKTA OIDC authentication. clean up of old tutorial examples.

--- src/main/java/gov/ny/its/mentor_demo/MentorDemoApplication.java
+++ src/main/java/gov/ny/its/mentor_demo/MentorDemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MentorDemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MentorDemoApplication.class, args);
 	}
-
 }

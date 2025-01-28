2b7820558a9ab918a9d561d9de38becaf028592a
Christopher Walsh <chris.is.rad@pm.me>
2025-01-28 11:01:12 -0500

    test case for live HTTP requests. also a serverless test case. removing Hibernate schema reverse engineering for now

--- src/main/java/gov/ny/its/mentor_demo/controller/HomeController.java
+++ src/main/java/gov/ny/its/mentor_demo/controller/HomeController.java
@@ -5,12 +5,22 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 	@RequestMapping("/")
 	public String index() {
 		return "index";
+	}
+
+	/**
+	 * for smoke-testing and endpoint(s) (see test packages)
+	 * @return a string
+	 */
+	@GetMapping("/smoke-test")
+	public @ResponseBody String smokeTest() {
+		return "Hello, World";
 	}
 
 	@GetMapping("/user/login")

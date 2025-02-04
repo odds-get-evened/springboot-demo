ecf80b0fe894c65983c58cc21f5272a23c8d995b
Christopher Walsh <chris.is.rad@pm.me>
2025-02-04 09:49:30 -0500

    just for demonstration

--- src/main/java/gov/ny/its/mentor_demo/controller/UserController.java
+++ src/main/java/gov/ny/its/mentor_demo/controller/UserController.java
@@ -1,12 +1,25 @@
 package gov.ny.its.mentor_demo.controller;
 
+import gov.ny.its.mentor_demo.entity.UserEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 	@GetMapping("/account/register")
-	public String registration() {
+	public String registration(Model model) {
+		model.addAttribute("user", new UserEntity());
+
 		return "account/register";
+	}
+
+	@PostMapping("/account/register")
+	public String procRegistration(@ModelAttribute UserEntity user, Model model) {
+		model.addAttribute("user", user);
+
+		return "account/register-success";
 	}
 }

ea028c9d44688f3299cd4093c7eb5615f376603c
Christopher Walsh <chris.is.rad@pm.me>
2025-02-07 15:16:50 -0500

    stopping point with logout. cannot seem to get rid of the auth0 middleman confirmation page

--- src/main/java/gov/ny/its/mentor_demo/security/MentorSecurityConfig.java
+++ src/main/java/gov/ny/its/mentor_demo/security/MentorSecurityConfig.java
@@ -1,11 +1,17 @@
 package gov.ny.its.mentor_demo.security;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 
 /**
  * primary configuration for application security
@@ -13,14 +19,31 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class MentorSecurityConfig {
+	private final Environment environment;
+
+	public MentorSecurityConfig(Environment environment) {
+		this.environment = environment;
+	}
+
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity sec) throws Exception {
 		sec.authorizeHttpRequests((auth) -> {
-			auth.requestMatchers("/login/**", "/account/register").permitAll();
+			auth.requestMatchers("/login/**", "/account/register", "/logout").permitAll();
 			auth.requestMatchers("/secret/**").authenticated();
 			auth.anyRequest().permitAll();
 		});
 
+		/*
+		 * @todo bypass the auth0 logout confirmation (defaults to this for some reason)
+		 */
+		sec.logout((logout) -> {
+			logout.logoutUrl("/logout");
+			logout.logoutSuccessUrl("/");
+			logout.invalidateHttpSession(true);
+			logout.deleteCookies("JSESSIONID");
+		});
+
+		// assign the Okta login to the security instance
 		sec.oauth2Login(Customizer.withDefaults());
 
 		return sec.build();
